5b5cb3603f384ba681f8c24d17694d5ca856885e
NHT <hoangtuan910@gmail.com>
2023-10-21 10:35:39 +0700

    change title & shortcut icon

--- layouts/404.tsx
+++ layouts/404.tsx
@@ -1,27 +1,26 @@
-import Head from 'next/head';
-import Header from 'components/header';
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import Header from "components/header";
+import { useRouter } from "next/router";
 
 type LayoutType = {
-    title?: string;
-    children?: React.ReactNode;
-}
+  title?: string;
+  children?: React.ReactNode;
+};
 
-export default ({ children, title = 'Next.js Ecommerce' }: LayoutType) => {
+export default ({ children, title = "2ALL" }: LayoutType) => {
   const router = useRouter();
   const pathname = router.pathname;
 
   return (
     <div className="app-main">
       <Head>
-        <title>Page not found &mdash; { title }</title>
+        <title>Page not found &mdash; {title}</title>
+        <link rel="shortcut icon" href="/images/Logo_without_name.svg" />
       </Head>
 
       <Header />
 
-      <main className={(pathname !== '/' ? 'main-page' : '')}>
-        { children }
-      </main>
+      <main className={pathname !== "/" ? "main-page" : ""}>{children}</main>
     </div>
-  )
-}
+  );
+};
