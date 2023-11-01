21f6a773fda2c35a95b30026fd4788471ecc2d94
NHT <hoangtuan910@gmail.com>
2023-11-01 14:27:53 +0700

    try to fix hardcode and time-safe string comparison

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,4 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
+import crypto from "crypto";
 
 // fake login
 export default (req: NextApiRequest, res: NextApiResponse) => {
@@ -6,9 +7,12 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   const email = request.email;
   const password = request.password;
 
-  if(email === 'johndoe@mail.com' && password === 'ecommerce') {
+  const dbEmail = "johndoe@mail.com";
+  const dbPassword = Buffer.alloc(1020, "ecommerce");
+
+  if (email === dbEmail && crypto.timingSafeEqual(password, dbPassword)) {
     res.status(200).json({ status: true });
   } else {
     res.status(401).json({ status: false });
   }
-}
+};
